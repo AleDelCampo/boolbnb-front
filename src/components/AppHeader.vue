0a93d85b3e4a5ce52ced6d0e3d3edcf994adc408
GiuseppeTrp <trapassogiuseppe158@gmail.com>
2024-06-01 17:04:43 +0200

    position fixed navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,8 +14,8 @@ export default {
 
 <template>
 
-    <header>
-        <nav class="navbar navbar-expand-lg">
+    <header class="mb-5">
+        <nav class="navbar navbar-expand-lg fixed-top">
             <div class="container">
                 <a class="navbar-brand me-5" href="#">
                     <router-link :to="{ name: 'HomePage' }">
